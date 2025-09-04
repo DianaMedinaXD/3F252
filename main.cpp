c37f0c09751b8b32e3cba71523a1006e20c40233
DianaMedinaXD <a24310331@live.ceti.mx>
2025-09-04 23:31:40 +0000

    Mi primer commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<Foco.hpp>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout<<"Hola mundo"<<endl;
+
+    Foco foco;
+    foco.Encender();
+    cout<<"EstadoFoco:"
+    <<foco.LeerEstado()<<endl;
+    return 0;
+}
