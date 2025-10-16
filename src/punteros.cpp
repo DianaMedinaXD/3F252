1ab7a0c33b7e54c59899f1efa857223960dd77cf
DianaMedinaXD <a24310331@live.ceti.mx>
2025-10-16 18:05:54 +0000

    1

--- src/punteros.cpp
+++ src/punteros.cpp
@@ -8,22 +8,41 @@ class Humano{
         string nombre;
         int edad;
     public:
-        string LeerNombre();
-        int LeerEdad();
+        string LeerNombre(){
+            cin>>nombre;
+            return nombre;
+        }
+        int LeerEdad(){
+            cin>>edad;
+            return edad;
+        }
+        Humano(){
+            nombre = "sin_nombre";
+            edad = 0;
+        }
+        
 };
 
 class Alumno : public Humano{
     private:
         int registro;
     public:
-        int LeerRegistro();
+        int LeerRegistro(){
+            cin>>registro;
+            return registro;
+        }
+        Alumno(){registro = 0;}
 };
 
 class Empleado : public Humano{
     private:
         long noEmpleado;
     public:
-        long LeerNumeroEmpleado();    
+        long LeerNumeroEmpleado(){
+            cin>>noEmpleado;
+            return noEmpleado;
+        }
+        Empleado(){noEmpleado = 0;}    
 };
 
 int main(int argc, char const *argv[])
@@ -33,10 +52,16 @@ int main(int argc, char const *argv[])
     Empleado* empleado = new Empleado();
 
     humano = alumno;
+    cout << "Edad alumno: ";
     humano->LeerEdad();
+    cout << "Nombre alumno: ";
+    humano->LeerNombre();
 
     humano = empleado;
+    cout << "Edad empleado: ";
     humano->LeerEdad(); //cuando tienes muchos objetos y todos tienen una misma caracteristica
+    cout << "Nombre emleado: ";
+    humano->LeerNombre();
 
     list<Humano*> listaHumanos;
     listaHumanos.emplace_back(new Alumno());
@@ -55,5 +80,13 @@ int main(int argc, char const *argv[])
         cout << humano->LeerNombre() << endl;
     }
 
+    // Liberar memoria
+    delete alumno;
+    delete empleado;
+    for (auto&& humano : listaHumanos)
+    {
+        delete humano;
+    }
+    
     return 0;
 }
